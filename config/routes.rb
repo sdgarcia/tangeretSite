3050609c39b8281b022ad62d0f2ea67b18f5db2b
Sebastian Garcia <sdgarcia@gmail.com>
2012-10-28 23:05:54 -0300

    fixing issues with js and zoom.png

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TangeretSite::Application.routes.draw do
  root to: 'static_pages#home'
 
+  match '/index', to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/products', to: 'static_pages#products'
   match '/services', to: 'static_pages#services'
