06f67fc5bc782fd3098c326565285fc19b218a8e
Sebastian Garcia <sdgarcia@gmail.com>
2012-11-02 14:44:55 -0300

    remove index and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 TangeretSite::Application.routes.draw do
- root to: 'static_pages#home'
+ root to: "static_pages#home"
 
-  match '/index', to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/products', to: 'static_pages#products'
   match '/services', to: 'static_pages#services'
